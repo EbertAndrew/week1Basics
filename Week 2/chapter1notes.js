99be436ccc0b6bfc348bcd5d94c07b8ece1f3638
Andrew Ebert <andrewsebertii@gmail.com>
2020-05-04 15:16:55 -0500

    chapter1notes

---
+++ Week 2/chapter1notes.js
@@ -0,0 +1,18 @@
+var winners =       2      ;
+// Create a variable with var keyword. var variableName = somevalue;
+var name = "Duke";
+
+var isEligible = false;
+
+var nothingHere;
+
+var allGone;
+
+var _thisIsGood;
+
+var $soIsThis;
+
+
+
+// These are not
+// Cannot use break, continue, interface, return, public, and more See page 12
