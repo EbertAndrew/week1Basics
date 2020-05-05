265421c58664db1667c0f307f5b5342747ef083f
Andrew Ebert <andrewsebertii@gmail.com>
2020-05-05 15:16:55 -0500

    NewSong

---
+++ Week 2/hmwrk3.js
@@ -0,0 +1,19 @@
+var word = "absurd";
+
+    var count = 500;
+
+    while (count > 0)   {
+        console.log(count + " " + word + " amounts of lag in the console");
+        console.log(count + " " + word + " amounts of lag,");
+        console.log("Process some out, load a string,");
+        count = count - 1;
+
+    if (count > 0)  {
+
+        console.log(count + " " + word + " amounts of lag in the console.");
+        
+    }   else {
+        console.log("No more " + word + " amounts of lag in the console.");
+    }
+    
+}
