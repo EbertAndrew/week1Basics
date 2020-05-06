a62582a7da8f638d3954463e4c40c9d3e8cbb6ff
Andrew Ebert <andrewsebertii@gmail.com>
2020-05-06 15:36:15 -0500

    dataTypesConsoled

---
+++ Week 2/dataTypes.js
@@ -0,0 +1,94 @@
+// numbers
+var numx = 5;
+var y = 8;
+var z = 12;
+console.log("numx");
+
+
+// Strings
+
+var strx = " Andrew E";
+var y = " Family of 38";
+var z = " Love Mine ";
+console.log(strx);
+
+// Decimals
+
+var decx = 10.4;
+var y = 22.8;
+var z = 0.6;
+console.log(decx);
+
+// Scientific Notation
+
+var snx = 345e5;
+var y = 123e7;
+var z = 503e8;
+console.log(snx);
+
+// Boolean
+
+var boox = true;
+var y = false;
+console.log(boox);
+
+// Arrays
+var arx = [1,2,3,4];
+var y = ["hello", "world"];
+console.log(arx);
+
+// Object
+
+var objhouse = {
+    doors : "one",
+    windows : "two",
+    garages : "three",
+}
+console.log(objhouse);
+
+var car = {
+    doors : "two",
+    windows : true,
+    garages : "steelblue",
+}
+
+// Undefined Objects
+
+var uox;
+var y;
+var z;
+console.log(uox);
+
+// Empty Object
+
+var eox = null;
+var y = null;
+var z = null;
+console.log(eox);
+
+// Type Of Operator
+// Find the type of a variable
+// Type string
+var osstr = " this string ";
+var result = typeof(str);
+console.log(osstr);
+
+// Type number
+var nnum = 94843;
+var result2 = typeof(num);
+console.log(nnum);
+
+// Type Boolean
+var bboo = true;
+var result3 = typeof(boo);
+console.log(bboo);
+
+// Type Null
+var nempty = null;
+var result4 = typeof(empty);
+console.log(nempty);
+
+// Type Undefined
+var undundefy;
+var result5 = typeof(undefy);
+console.log(undundefy);
