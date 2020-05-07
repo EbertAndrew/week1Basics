50e217c551bf8db2a9b168ef6a43970504a980dd
Andrew Ebert <andrewsebertii@gmail.com>
2020-05-07 09:38:31 -0500

    questionOfDay

---
+++ Week 2/questionOfDayDataTypes.js
@@ -0,0 +1,20 @@
+// numbers
+var numx = 76;
+var y = 9;
+var z = 34;
+console.log("numx");
+
+
+// Strings
+
+var strx = " Devilsaur";
+var y = " Googling Balloons";
+var z = " Trunkles ";
+console.log(strx);
+
+// Decimals
+
+var decx = 10.9;
+var y = 2.34;
+var z = 0.753;
+console.log(decx);
