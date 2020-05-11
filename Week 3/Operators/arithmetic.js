6846c86497fe5cf16b4469a1ae5406b2e514470a
Andrew Ebert <andrewsebertii@gmail.com>
2020-05-11 15:45:26 -0500

    Operators

---
+++ Week 3/Operators/arithmetic.js
@@ -0,0 +1,33 @@
+// Arithmetic Operators
+
+// Operands , are simply variables.
+var x = 149;
+console.log("The value of x is : " + x)
+var y = 260;
+console.log("The value of y is : " + y)
+
+// Addition
+result = x + y;
+console.log("Addition : " + result);
+
+// Subtraction
+result = x - y;
+console.log("Subtraction" + result);
+
+// Multiplication
+result = x * y;
+console.log("Multiplication : " + result);
+
+// Division
+result = x / y;
+console.log("Division : " + result);
+
+// Remainder
+result = x % y;
+console.log("Remainder : " + result);
+
+// Increment && Decrement
+result = ++x;
+console.log(result)
+result = --y;
+console.log(result)
