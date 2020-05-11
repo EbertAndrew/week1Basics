ad377a0f02d5f16ba4275a725824b792807324c9
Andrew Ebert <andrewsebertii@gmail.com>
2020-05-11 14:34:17 -0500

    variables

---
+++ Week 3/variables.js
@@ -0,0 +1,41 @@
+// First we need the Var keyword
+var thisVariable;
+document.getElementById("demo").innerHTML = thisVariable; // This is the first link.
+
+var anotherOne;
+
+var the1LastTrain;
+
+//  Next we need a name
+var myVariable // Declare a Variable
+
+// Next we need to add a Data Type
+var myVariable = 100 ; //  Instantiate Value , set a value to myVariable
+
+// Change the Value of myVariable
+myVariable = " One Hundred ";
+
+thisVariable = "money";
+document.getElementById("demo2").innerHTML = thisVariable;
+
+thisVariable = 3.33 ;
+document.getElementById("demo3").innerHTML = thisVariable;
+
+thisVariable = 45.64;
+document.getElementById("demo4").innerHTML = thisVariable;
+
+myVariable = 11e3;
+
+anotherOne = true
+
+the1LastTrain = [1,2,3,4,5,6];
+
+the1LastTrain = ["one", "two", "three"];
+
+anotherOne = {
+    name: "ramone",
+    favColor: "blue"
+}
+
+thisVariable = undefined;
+document.getElementById("demo5").innerHTML = thisVariable;
