826fc085f6f4021ca884e52c2c4d946879e21a82
Andrew Ebert <andrewsebertii@gmail.com>
2020-05-18 15:42:56 -0500

    objHomework

---
+++ Week 4/objHomework.js
@@ -0,0 +1,64 @@
+var enomaDoor = {
+    material: "Cedar",
+    color: "brown",
+    type: "wood",
+    vintage ? : true,
+    knocker ? : true,
+    size: 10,
+    brand: undefined
+}
+
+// printing with an object and changing the properties.
+console.log(enomaDoor); // Print Object
+console.log(enomaDoor.brand); // See a property value
+enomaShoe.brand = "enoma"; // Change the value to something else.
+console.log("...... Loading ......") // Added loading in for fun
+console.log(enomaDoor) // Print object again
+
+// expand object with new properties.
+enomaShoe.price = $15000;
+console.log(enomaDoor);
+
+var enomaBall = {
+    material: "leather",
+    color: "white",
+    type: "baseball",
+    vintage ? : false,
+    laces ? : true,
+    size: 4,
+    brand: undefined
+}
+
+// printing with an object and changing the properties.
+console.log(enomaBall); // Print Object
+console.log(enomaBall.brand); // See a property value
+enomaBall.brand = "enoma"; // Change the value to something else.
+console.log("...... Loading ......") // Added loading in for fun
+console.log(enomaBall) // Print object again
+
+// expand object with new properties.
+enomaBall.price = $7;
+console.log(enomaBall);
+
+var enomaHouse = {
+    material: "meteor",
+    color: "crystal ash",
+    type: "gargantuan",
+    vintage ? : false,
+    impassable ? : true,
+    size: 20,
+    brand: undefined
+}
+
+// printing with an object and changing the properties.
+console.log(enomaHouse); // Print Object
+console.log(enomaHouse.brand); // See a property value
+enomaHouse.brand = "enoma"; // Change the value to something else.
+console.log("...... Loading ......") // Added loading in for fun
+console.log(enomaHouse) // Print object again
+
+// expand object with new properties.
+enomaHouse.price = $4500000000;
+console.log(enomaHouse);
+
+
